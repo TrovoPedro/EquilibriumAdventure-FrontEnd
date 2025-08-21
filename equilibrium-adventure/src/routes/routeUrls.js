b242642a5c68adcbd2a6e5dfd097d7bd3dd53a13
TrovoPedro <pedrotrovo05@outlook.com>
2025-08-21 20:04:16 -0300

    Criação e finalização  da pagina de editar eventos

--- equilibrium-adventure/src/routes/routeUrls.js
+++ equilibrium-adventure/src/routes/routeUrls.js
@@ -2,6 +2,7 @@ const routeUrls = {
     HOME: '/',
     LOGIN: '/login',
     CRIAR_EVENTO: '/criar-evento',
+    EDITAR_EVENTO: '/editar-evento',
     CADASTRO: '/cadastro',
     DASHBOARD: '/dashboard',
     ESCOLHER_GUIA: '/escolher-guia'
