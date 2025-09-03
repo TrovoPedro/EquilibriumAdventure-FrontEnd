25a2bef650add9a0ceb38255e9af37fd258ac520
EduardoFacini <eduardo.dorval@sptech.school>
2025-09-03 18:47:37 -0300

    Criação da tela de agenda do aventureiro

---
+++ equilibrium-adventure/src/pages/agenda-aventureiro/agenda-aventureiro.jsx
@@ -0,0 +1,13 @@
+import Button_Voltar from "../../components/circle-back-button/circle-back-button";
+
+const CriarAgendaAventureiro = () => {
+    return (
+        <>
+        <Button_Voltar/>
+            <div className="home-container">
+                
+            </div>
+        </>
+    )
+}
+export default CriarAgendaAventureiro
