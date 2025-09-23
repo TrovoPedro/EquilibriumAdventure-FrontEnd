e079d99e52cce38f582f08d6762c9bb0bb432e2e
edro01pinho <pedro.antunes@sptech.school>
2025-09-23 16:19:51 -0300

    Refatoração da página dados-guia

--- equilibrium-adventure/src/pages/dados-guia/dados-guia.jsx
+++ equilibrium-adventure/src/pages/dados-guia/dados-guia.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./dados-guia.css";
 import { useNavigate } from "react-router-dom";
+import routeUrls from "../../routes/routeUrls";
 import BackButton from "../../components/circle-back-button/circle-back-button";
 import { FaCloudUploadAlt } from "react-icons/fa";
 
@@ -31,56 +32,64 @@ export default function DadosGuia() {
         navigate(routeUrls.HOME);
     };
 
+
+
     return (
         <>
             <div className="dados-guia-wrapper">
                 <BackButton />
                 <div className="dados-guia-header">Dados do Guia</div>
                 <div className="dados-guia-container">
-                    <label htmlFor="">
-                        Nome:
-                        <input type="text" name="nome" />
-                    </label>
-                    <label htmlFor="">
-                        Email:
-                        <input type="email" name="email" />
-                    </label>
+                    <div className="dados-guia-body">
 
-                    <label id="imagem-label" htmlFor="">
-                        Imagem:
-                        <div className="adicionar-guia-imagem">
-                            <div
-                                className="upload-box"
-                                onClick={() => document.getElementById("upload-input").click()}
-                            >
-                                {formData.imagem ? (
-                                    <img
-                                        src={URL.createObjectURL(formData.imagem)}
-                                        alt="Pré-visualização"
-                                        className="preview-img"
-                                    />
-                                ) : (
-                                    <div className="upload-placeholder">
-                                        <FaCloudUploadAlt size={150} color="#0C513F" />
-                                        <p>Clique ou arraste uma imagem aqui</p>
-                                    </div>
-                                )}
+                        <label htmlFor="">
+                            Nome:
+                            <input type="text" name="nome" />
+                        </label>
+                        <label htmlFor="">
+                            Email:
+                            <input type="email" name="email" />
+                        </label>
+
+                        <label id="imagem-label" htmlFor="">
+                            Imagem:
+                            <div className="adicionar-guia-imagem">
+                                <div
+                                    className="upload-box"
+                                    onClick={() => document.getElementById("upload-input").click()}
+                                >
+                                    {formData.imagem ? (
+                                        <img
+                                            src={URL.createObjectURL(formData.imagem)}
+                                            alt="Pré-visualização"
+                                            className="preview-img"
+                                        />
+                                    ) : (
+                                        <div className="upload-placeholder">
+                                            <FaCloudUploadAlt size={150} color="#0C513F" />
+                                            <p>Clique ou arraste uma imagem aqui</p>
+                                        </div>
+                                    )}
+                                </div>
+                                <input
+                                    type="file"
+                                    id="upload-input"
+                                    name="imagem"
+                                    onChange={handleChange}
+                                    accept="image/*"
+                                    style={{ display: "none" }}
+                                />
+
                             </div>
-                            <input
-                                type="file"
-                                id="upload-input"
-                                name="imagem"
-                                onChange={handleChange}
-                                accept="image/*"
-                                style={{ display: "none" }}
-                            />
-
-                        </div>
-                    </label>
-                    <label id="descricao-label" htmlFor="">
-                        Descrição do Guia:
-                        <textarea name="descricao" id="descricao" value={formData.descricao} onChange={handleChange}></textarea>
-                    </label>
+                        </label>
+                        <label id="descricao-label" htmlFor="">
+                            Descrição do Guia:
+                            <textarea name="descricao" id="descricao" value={formData.descricao} onChange={handleChange}></textarea>
+                        </label>
+                    </div>
+                    <div className="dados-guia-bottom">
+                        <button className="dados-guia-btn">Salvar Alterações</button>
+                    </div>
                 </div>
             </div>
         </>
