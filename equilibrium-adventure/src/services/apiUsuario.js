b9e8063d5368dcae3182232d68569a43956c66e9
RebecaCagni <rebeca.cagni@sptech.school>
2025-10-08 23:49:30 -0300

    Integração do componente de header para aparecer foto do usuário

--- equilibrium-adventure/src/services/apiUsuario.js
+++ equilibrium-adventure/src/services/apiUsuario.js
@@ -16,3 +16,16 @@ export const buscarDadosUsuario = async (id) => {
     }
 };
 
+export async function buscarImagemUsuario(idUsuario) {
+  try {
+    const response = await api.get(`/informacoes-pessoais/${idUsuario}/imagem`, {
+      responseType: "blob",
+    });
+
+    // Converte o blob em uma URL utilizável no <img>
+    return URL.createObjectURL(response.data);
+  } catch (error) {
+    console.error("Erro ao buscar imagem do usuário:", error);
+    return null;
+  }
+}
