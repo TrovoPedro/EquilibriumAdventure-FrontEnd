0a7f74601075b3371491ccbc456061ea112505c2
PedroTrovoSouza <pedro.tsouza@solutis.com.br>
2025-10-12 22:26:45 -0300

    integração da agenda do guia finalizada

--- equilibrium-adventure/src/services/chamadasAPIAgenda.js
+++ equilibrium-adventure/src/services/chamadasAPIAgenda.js
@@ -1,46 +1,36 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/agendamentos";
+const BASE_URL_AGENDAS = "http://localhost:8080/agendas";
 
 export async function buscarDatasDisponiveis() {
   try {
-    const response = await axios.get(`${BASE_URL}/datas-disponiveis`);
+    const response = await axios.get(`${BASE_URL_AGENDAS}/disponiveis`);
     return response.data;
   } catch (err) {
-    console.error("Erro ao buscar datas disponíveis:", err);
+    console.error("Erro ao buscar datas disponíveis:", err.response?.data || err.message);
     throw err;
   }
 }
 
-export const agendarAnamnese = async ({ dataId, aventureiroId, horario }) => {
+export async function adicionarDisponibilidade({ fkGuia = 1, dataDisponivel }) {
   try {
-    const response = await axios.post(`${BASE_URL}/agendar`, {
-      agendaResponsavel: { id: dataId },
-      aventureiro: { id: aventureiroId },
-      horario: horario
+    const response = await axios.post(`${BASE_URL_AGENDAS}`, {
+      fkGuia,
+      dataDisponivel,
     });
     return response.data;
   } catch (err) {
-    console.log('====================================');
-    console.log("dataId:", dataId);
-    console.log("aventureiroId:", aventureiroId);
-    console.log("horario:", horario);
-    console.log('====================================');
-    console.error("Erro ao agendar:", err.response?.data || err.message);
+    console.error("Erro ao adicionar disponibilidade:", err.response?.data || err.message);
     throw err;
   }
-};
+}
 
-export const gerarRelatorioAnamnese = async ({ userId, relatorio }) => {
+export async function listarAgenda() {
   try {
-    // const response = await axios.patch(`${BASE_URL}/gerar-relatorio`, {
-    //   fkAventureiro: userId,
-    //   descricao: relatorio
-    // });
-    const response = await axios.patch(`${BASE_URL}/gerar-relatorio?fkAventureiro=${userId}&descricao=${encodeURIComponent(relatorio)}`);
+    const response = await axios.get(`${BASE_URL_AGENDAS}`);
     return response.data;
   } catch (err) {
-    console.error("Erro ao gerar relatório de anamnese:", err);
+    console.error("Erro ao listar agenda:", err.response?.data || err.message);
     throw err;
   }
-};
+}
